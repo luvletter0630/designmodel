0883d0150615e7a5957e134c1dd61373c11ccd91
liwj <761644814@qq.com>
2021-04-06 11:15:22 +0800

    adapter

---
+++ src/lwj/study/designmodel/AdapterPattern/IVoltage5V.java
@@ -0,0 +1,5 @@
+package lwj.study.designmodel.AdapterPattern;
+
+public interface IVoltage5V {
+    int Output5v();
+}
---
+++ src/lwj/study/designmodel/AdapterPattern/Voltage220V.java
@@ -0,0 +1,9 @@
+package lwj.study.designmodel.AdapterPattern;
+
+public class Voltage220V {
+    private final int voltage = 220;
+
+    public int getVoltage() {
+        return voltage;
+    }
+}
